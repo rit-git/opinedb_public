30d113b1ccd824cd73d6c47773583897a8e21640
Ubuntu <ubuntu@ip-172-31-8-101.us-west-2.compute.internal>
2019-05-17 12:11:45 +0000

    bug fix in opine sql by vincent

--- sql/opine_sql.py
+++ sql/opine_sql.py
@@ -118,7 +118,7 @@ class SimpleOpine(object):
 
     def cosine(self, vec1, vec2):
         norm1 = np.linalg.norm(vec1)
-        norm2 = np.linalg.norm(vec1)
+        norm2 = np.linalg.norm(vec2)
         if norm1 > 0 and norm2 > 0:
             return np.dot(vec1, vec2) / norm1 / norm2
         else:
